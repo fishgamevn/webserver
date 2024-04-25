0341d17ff3ac7724ab47fc63d7db42c813b2b3d3
fishgamevn <165646858+fishgamevn@users.noreply.github.com>
2024-04-25 17:53:48 +0700

    Update log.js

--- log.js
+++ log.js
@@ -19,3 +19,8 @@ logInfo: (...message) => {
     logsMessage.push(JSON.stringify(message));
     if (seft.canLog("info")) logger.info(...message);
 },
+
+  https://stackabuse.com/bytes/how-to-kill-a-process-using-a-port-in-linux/
+https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-centos-8
+https://www.digitalocean.com/community/tutorials/initial-server-setup-with-centos
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-centos-8
